695c716ac550a5d2104f9be57789d33126c7895a
priyasaravanan143 <banupriyasaravanan1234@gmail.com>
2019-12-10 14:45:12 +0530

    add code

---
+++ src/variables/TypesOfVariables.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class TypesOfVariables {
+	int a=10;
+private void add() {
+System.out.println(a);
+
+}
+public static void main(String[] args) {
+	TypesOfVariables t=new TypesOfVariables();
+	System.out.println(t.a);
+}
+}
